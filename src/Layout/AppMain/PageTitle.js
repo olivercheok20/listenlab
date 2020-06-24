ddecd30e1f07c814d1d2060a5b7051d6814988ff
olivercheok20 <olivercheok20@gmail.com>
2020-06-24 15:24:47 +0800

    Change page title and remove unnecessary code

--- src/Layout/AppMain/PageTitle.js
+++ src/Layout/AppMain/PageTitle.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import cx from "classnames";
 
-import TitleComponent1 from "./PageTitleExamples/Variation1";
-import TitleComponent2 from "./PageTitleExamples/Variation2";
-import TitleComponent3 from "./PageTitleExamples/Variation3";
-
 class PageTitle extends Component {
   randomize(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
@@ -21,27 +17,24 @@ class PageTitle extends Component {
       subheading,
     } = this.props;
 
-    var arr = [<TitleComponent1 />, <TitleComponent2 />, <TitleComponent3 />];
-
     return (
       <div className="app-page-title">
         <div className="page-title-wrapper">
           <div className="page-title-heading">
             <div className={cx("page-title-icon", {
-                "d-none": !enablePageTitleIcon,
-              })}>
+              "d-none": !enablePageTitleIcon,
+            })}>
               <i className={icon} />
             </div>
             <div>
               {heading}
               <div className={cx("page-title-subheading", {
-                  "d-none": !enablePageTitleSubheading,
-                })}>
+                "d-none": !enablePageTitleSubheading,
+              })}>
                 {subheading}
               </div>
             </div>
           </div>
-          <div className="page-title-actions">{this.randomize(arr)}</div>
         </div>
       </div>
     );
